c911acba4884fba78b0af846f753574206f621b8
sideeg <sideegmohammednoor@gmail.com>
2023-09-05 21:23:53 +0000

    add folder architecture

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask, redirect, url_for, request, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def landing_page():
+   return render_template('hello.html')
+
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+   if request.method == 'POST':
+      user = request.form['nm']
+      return render_template('login.html')
+   else:
+      user = request.args.get('nm')
+      return render_template('login.html')
+
+@app.route('/register',methods = ['POST', 'GET'])
+def register():
+   if request.method == 'POST':
+      #user = request.form['nm']
+      return render_template('register.html')
+   else:
+      #user = request.args.get('nm')
+      return render_template('register.html')
+
+
+if __name__ == '__main__':
+   app.run('0.0.0.0',5000,True)
