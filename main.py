939da5d7ae82ad4d6e9fe4751064bc34c0a2a80c
sideeg <sideegmohammednoor@gmail.com>
2023-09-12 11:31:27 +0000

    add comments to main.py and refactor the code

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from flask import Flask, redirect, url_for, request, render_template
-# import flaskext_compat
-# flaskext_compat.activate()
-# from flask.ext import foo
+
 import sqlite3
 app = Flask(__name__)
 
+#route to the db file
 db_file = "/home/ubuntu/easy_move/db/easy_move.db"
+
+#the main route 
 @app.route('/')
 def landing_page():
         return render_template("index.html")
 
+#the login routes for get and post
 @app.route('/login',methods = ['POST', 'GET'])
 def login():
    if request.method == 'POST':
@@ -17,6 +19,7 @@ def login():
    else:
       return render_template('login.html')
 
+#the register route for the post and get
 @app.route('/register',methods = ['POST', 'GET'])
 def register():
    if request.method == 'POST':
@@ -45,8 +48,11 @@ def register():
    else:
       return render_template('register.html')
 
+#the main page which have all the feature
 @app.route('/home')
 def home():
      return render_template('home.html')
+
+
 if __name__ == '__main__':
    app.run('0.0.0.0',5000,True)
