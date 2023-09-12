7284c6638d659a87c57b589084f709001455863a
sideeg <sideegmohammednoor@gmail.com>
2023-09-12 12:39:36 +0000

    add method to get the lat long of the user

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from flask import Flask, redirect, url_for, request, render_template
-
+from simple_geoip import GeoIP
+from flask import jsonify
 import sqlite3
 app = Flask(__name__)
 
 #route to the db file
 db_file = "/home/ubuntu/easy_move/db/easy_move.db"
-
+#prepare the geoip key
+geoip = GeoIP("at_1XijkKLvn8aL4z8c7SKM2EdL0UR6E");
 #the main route 
 @app.route('/')
 def landing_page():
@@ -49,10 +51,30 @@ def register():
       return render_template('register.html')
 
 #the main page which have all the feature
+
 @app.route('/home')
 def home():
      return render_template('home.html')
 
-
+#get the user cruunt location
+@app.route('/get_location')
+def get_location():
+      
+    try:
+       data = geoip.lookup(str(request.remote_addr)) # Replace this with your public IP
+    except ConnectionError:
+       # If you get here, it means you were unable to reach the geoipify
+       # service, most likely because of a network error on your end.
+       return 'ConnectionError'
+       #except ServiceError:
+       # If you get here, it means geoipify is having issues, so the request
+       # couldn't be completed 
+       #return 'ServiceError'
+    except:
+       # Something else happened (non-geoipify) related. Maybe you hit CTRL-C
+       # while the program was running, the kernel is killing your process, or
+       # something else all together.
+       return 'sonting went wrong'
+    return data
 if __name__ == '__main__':
    app.run('0.0.0.0',5000,True)
